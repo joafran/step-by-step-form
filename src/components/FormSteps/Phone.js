08486401a54a8019a24091dd0d462db15a6a2eba
joafran <joafran0016@gmail.com>
2021-06-11 14:48:33 -0300

    added regex validation in phone number

--- src/components/FormSteps/Phone.js
+++ src/components/FormSteps/Phone.js
@@ -7,7 +7,7 @@ const PhoneNumber = () => {
     return ( 
         <fieldset className="d-flex flex-column">
             <label className="mb-2">What phone number can we reach you at?</label>
-            <input value={formState.phoneNumber} name="phoneNumber" className="form-control" placeholder="(000) 000-0000" type="tel" minLength="10" maxLength="10" required onChange={ (e) => updateForm(e)} />
+            <input value={formState.phoneNumber} name="phoneNumber" className="form-control" placeholder="(000) 000-0000" type="tel" pattern="^[0-9]+" minLength="10" maxLength="10" required onChange={ (e) => updateForm(e)} />
             <p className="text-secondary mt-2" style={{fontSize: 12, opacity: 0.6}}>
              When you click "Next" you authorize companies on <a href="https://www.smartsolarsavings.com/v4/installers.php" target="blank" className="link-secondary">this list</a> to call and send pre-recorded
              and sms messages to phone # above using an autodialer even if your phone number is on any do not call list.
